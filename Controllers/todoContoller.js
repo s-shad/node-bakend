9495c3d4486d29737fe2876b0a31fd6360b91155
s-shad <9012606022a>
2022-04-15 13:14:28 +0430

    delete extera console log

--- Controllers/todoContoller.js
+++ Controllers/todoContoller.js
@@ -33,7 +33,6 @@ exports.complateTodo = async (req, res) => {
 exports.getIndex = async (req, res) => {
 	const completedTodos = await Todo.countDocuments({ completed: true });
 	const todos = await Todo.find();
-	console.log(todos);
 	res.render('index', {
 		pageTitle: 'کارهای روزمره',
 		todos,
